058358870a9979d91cecfcb74f5eef04f581adee
fyui001 <yun.cod@gmail.com>
2021-06-11 21:44:58 +0900

    テーブルの整地

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -1,23 +1,26 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
     public function run()
     {
         DB::table('users')->insert([
-            'user_id' => 'matsui_eriko',
+            'mail_address' => 'takada_yuki@kohinatamiho.com',
             'password' => Hash::make('hogehoge'),
-            'name' => '松井恵理子',
-            'access_token' => '',
-            'del_flg' => 0,
+            'user_name' => '高田憂希',
+            'status' => 1,
+            'role' => 0,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
