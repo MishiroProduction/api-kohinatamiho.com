dcc705bea78f57bb304430f42b9ee5806e0d56c8
fyui001 <yun.cod@gmail.com>
2021-06-11 20:03:15 +0900

    trash

--- resources/views/layouts/partials/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($errors->any())
-<div data-notify="container" class="col-md-12 mr-auto ml-auto alert alert-danger alert-with-icon animated fadeInDown" role="alert">
-    @foreach ($errors->all() as $error)
-        <span class="oi oi-warning"></span>
-        {{ $error }}
-        <br/>
-    @endforeach
-</div>
-@endif
--- resources/views/top_page.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.base', ['activePage' => 'TopPage', 'pageTitle' => 'トップページ'])
-@section('content')
-<p>トップページ</p>
-@endsection
