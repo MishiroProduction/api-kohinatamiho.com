0714c0d5f02c388b0ff63d67c6c1cfd37a03b1a7
fyui001 <yun.cod@gmail.com>
2021-06-20 02:46:39 +0900

    テスト環境を整えた

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+use App\Models\User;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createValidUserAccount(): User
+    {
+        $params = [
+            'mail_address' => genRandStr(200) . '@test.test',
+            'password' => 'password',
+            'user_name' => 'user_name',
+            'status' => rand(true, false),
+            'role' => rand(0, 2),
+        ];
+
+        return User::create($params);
+    }
 }
